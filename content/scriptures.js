384661454708dda214fbc19cbe27efba030a2118
jseffker <54945523+jseffker@users.noreply.github.com>
2026-05-19 13:03:08 -0600

    Revise

--- content/scriptures.js
+++ content/scriptures.js
@@ -1,7 +1,22 @@
 const scriptures = [
   {
+    "scripture": "...For the people of the Lord are they who wait for him; for they still wait for the coming of the Messiah.",
+    "reference": "2 Nephi 6:13",
+    "thought": ""
+  },
+  {
+    "scripture": "... they shall not be ashamed that wait for me.",
+    "reference": "2 Nephi 6: 7",
+    "thought": ""
+  },
+  {
     "scripture": "",
     "reference": "",
     "thought": ""
-  }
+  },
+  // {
+  //   "scripture": "",
+  //   "reference": "",
+  //   "thought": ""
+  // },
 ]
